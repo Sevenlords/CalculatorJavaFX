7b9faa48db9ff8b9b2a1dd3a0d5405111829db52
Vova <volodymyr.bunin@gmail.com>
2018-03-10 22:06:26 +0100

    Add viev

--- src/main/java/calculator/MainApp.java
+++ src/main/java/calculator/MainApp.java
@@ -1,4 +1,22 @@
 package calculator;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
