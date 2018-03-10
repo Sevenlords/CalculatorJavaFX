f9ee5b809552c2f64aeb371ce6e9308462ce0f3b
Vova <volodymyr.bunin@gmail.com>
2018-03-11 00:12:16 +0100

    Create calculator model

---
+++ src/main/java/calculator/model/MainModel.java
@@ -0,0 +1,47 @@
+package calculator.model;
+
+public class MainModel {
+
+    double calculated(double a, double b, String value) {
+        switch (value) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            case "x^y":
+                return Math.pow(a,b);
+            default:
+                return 0.0;
+        }
+    }
+
+    double calculated(double a, String value) {
+        switch (value) {
+            case "√":
+                return Math.sqrt(a);
+            case "1/x":
+                return 1 / a;
+            default:
+                return 0.0;
+        }
+    }
+
+    double percent(double a, double b, String value) {
+        switch (value) {
+            case "+":
+                return a + (b / 100 * a);
+            case "-":
+                return a - (b / 100 * a);
+            case "×":
+                return b / 100 * a;
+            case "÷":
+                return b / 100 * a;
+            default:
+                return 0.0;
+        }
+    }
+}
